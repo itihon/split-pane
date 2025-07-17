02498cfa7d1721bcb66795aed74047f47f98b329
itihon <kalabin.alexandr@gmail.com>
2025-07-17 14:51:19 +0500

    test: add removePane method

--- cypress/e2e/split-pane.cy.ts
+++ cypress/e2e/split-pane.cy.ts
@@ -70,6 +70,207 @@ describe('split-pane component', () => {
         });
     });
     
+    it('tests removePane method', () => {
+
+      const testRemovePanesFromEnd = (direction: SplitPaneOrientationType ) => (document: Document) => {
+        let panes;
+        let splitters;
+        const panesCount = 3;
+        const splitPane = createSplitPane(direction, panesCount);
+        document.body.appendChild(splitPane);
+
+        expect(splitPane.removePane(splitPane.length)).eq(false);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 1);
+        expect(splitPane.length).eq(panesCount);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr min-content 1fr min-content 1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 1');
+        expect(panes[1].textContent).eq('Content area 2');
+        expect(panes[2].textContent).eq('Content area 3');
+        expect(panes[3]).eq(undefined);
+
+        expect(splitPane.removePane(splitPane.length - 1)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 2);
+        expect(splitPane.length).eq(panesCount - 1);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr min-content 1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 1');
+        expect(panes[1].textContent).eq('Content area 2');
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+        
+        expect(splitPane.removePane(splitPane.length - 1)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 3);
+        expect(splitPane.length).eq(panesCount - 2);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 1');
+        expect(panes[1]).eq(undefined);
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+        
+        expect(splitPane.removePane(splitPane.length - 1)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 3);
+        expect(splitPane.length).eq(panesCount - 3);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0]).eq(undefined);
+        expect(panes[1]).eq(undefined);
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+        
+        expect(splitPane.removePane(splitPane.length - 1)).eq(false);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 3);
+        expect(splitPane.length).eq(panesCount - 3);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0]).eq(undefined);
+        expect(panes[1]).eq(undefined);
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+      };
+      
+      const testRemovePanesFromStart = (direction: SplitPaneOrientationType ) => (document: Document) => {
+        let panes;
+        let splitters;
+        const panesCount = 3;
+        const splitPane = createSplitPane(direction, panesCount);
+        document.body.appendChild(splitPane);
+
+        expect(splitPane.removePane(-1)).eq(false);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 1);
+        expect(splitPane.length).eq(panesCount);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr min-content 1fr min-content 1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 1');
+        expect(panes[1].textContent).eq('Content area 2');
+        expect(panes[2].textContent).eq('Content area 3');
+        expect(panes[3]).eq(undefined);
+
+        expect(splitPane.removePane(0)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 2);
+        expect(splitPane.length).eq(panesCount - 1);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr min-content 1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 2');
+        expect(panes[1].textContent).eq('Content area 3');
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+        
+        expect(splitPane.removePane(0)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 3);
+        expect(splitPane.length).eq(panesCount - 2);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 3');
+        expect(panes[1]).eq(undefined);
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+        
+        expect(splitPane.removePane(0)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 3);
+        expect(splitPane.length).eq(panesCount - 3);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0]).eq(undefined);
+        expect(panes[1]).eq(undefined);
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+        
+        expect(splitPane.removePane(0)).eq(false);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 3);
+        expect(splitPane.length).eq(panesCount - 3);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0]).eq(undefined);
+        expect(panes[1]).eq(undefined);
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+      };
+      
+      const testRemovePanesMiddle = (direction: SplitPaneOrientationType ) => (document: Document) => {
+        let panes;
+        let splitters;
+        const panesCount = 3;
+        const splitPane = createSplitPane(direction, panesCount);
+        document.body.appendChild(splitPane);
+
+        expect(splitPane.removePane(-1)).eq(false);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 1);
+        expect(splitPane.length).eq(panesCount);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr min-content 1fr min-content 1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 1');
+        expect(panes[1].textContent).eq('Content area 2');
+        expect(panes[2].textContent).eq('Content area 3');
+        expect(panes[3]).eq(undefined);
+
+        expect(splitPane.removePane(1)).eq(true);
+        panes = splitPane.getAllPanes();
+        splitters = splitPane.querySelectorAll('.sp-splitter');
+        expect(splitters.length).eq(panesCount - 2);
+        expect(splitPane.length).eq(panesCount - 1);
+        expect(splitPane.style.getPropertyValue('--grid-template'))
+          .eq('1fr min-content 1fr');
+        expect(panes[-1]).eq(undefined);
+        expect(panes[0].textContent).eq('Content area 1');
+        expect(panes[1].textContent).eq('Content area 3');
+        expect(panes[2]).eq(undefined);
+        expect(panes[3]).eq(undefined);
+      };
+
+      cy.document().then(removeAllSplitPanes);
+      cy.document().then(testRemovePanesFromEnd('horizontal'));
+      
+      cy.document().then(removeAllSplitPanes);
+      cy.document().then(testRemovePanesFromEnd('vertical'));
+      
+      cy.document().then(removeAllSplitPanes);
+      cy.document().then(testRemovePanesFromStart('horizontal'));
+      
+      cy.document().then(removeAllSplitPanes);
+      cy.document().then(testRemovePanesFromStart('vertical'));
+      
+      cy.document().then(removeAllSplitPanes);
+      cy.document().then(testRemovePanesMiddle('horizontal'));
+      
+      cy.document().then(removeAllSplitPanes);
+      cy.document().then(testRemovePanesMiddle('vertical'));
+    });
+
   });
 
 });
