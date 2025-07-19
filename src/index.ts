75737a4d15b0b29dd16b24e2fc30b0f780311c54
itihon <kalabin.alexandr@gmail.com>
2025-07-19 12:13:29 +0500

    refactor(index.ts): export state change event type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 import SplitPane from './SplitPane.js';
+import type SplitPaneStateChangeEvent from './SplitPaneStateChangeEvent.js';
+
 export default SplitPane;
+
+export type { SplitPaneStateChangeEvent };
