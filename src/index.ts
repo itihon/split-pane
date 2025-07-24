1905c45e2f660fb078bf2983ca6b283d5b4f5e46
itihon <kalabin.alexandr@gmail.com>
2025-07-24 17:03:04 +0500

    refactor(index.ts): remove no more necessary event class import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 import SplitPane from './SplitPane.js';
-import type SplitPaneStateChangeEvent from './SplitPaneStateChangeEvent.js';
-
 export default SplitPane;
-
-export type { SplitPaneStateChangeEvent };
